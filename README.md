22395843fdd1764260a9234084d23931f8abec19
Sem_lal@yahoo.com.hk <sem_lal@yahoo.com.hk>
2021-03-27 16:14:33 +0800

    updated mysql native password and create migration

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# consultaton_record
+# Development environment
+IDE - vsCode
+
+MySQL Community Server@8.0.23
+
+# backend
+---please update .env DB_NAME and DB_PASSWORD to your own setting---
+
+---run the following command for creating database in mysql---
+
+create database consultation_project;
+
+**please be aware nodeJs only support mysql native password**
---
+++ backend/migrations/20210327150126_init-db.ts
@@ -0,0 +1,32 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("client", (table) => {
+        table.increments();
+        table.timestamps(false, true);
+        table.string('email');
+        table.string('hashed_password');
+        table.string('clinic_name');
+        table.integer('phone_number');
+        table.string('address');
+    });
+    await knex.schema.createTable('consultation_record', (table) => {
+        table.increments();
+        table.timestamps(false, true);
+        table.string('clinic')
+        table.string('doctor_name')
+        table.string('patient_name')
+        table.string('diagnosis')
+        table.string('medication')
+        table.string('consultation fee')
+        table.string('follow_up_consultation')
+        table.dateTime('consultation_date_and_time');
+
+    });
+
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('client');
+}
