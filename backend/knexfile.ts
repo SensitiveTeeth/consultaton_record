e548c68cfab1116ff9ab5eb40db569a7c9b5b1f0
Sem_lal@yahoo.com.hk <sem_lal@yahoo.com.hk>
2021-03-27 14:22:28 +0800

    added knex

---
+++ backend/knexfile.ts
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+module.exports = {
+
+    development: {
+        client: 'mysql',
+        connection: {
+            database: process.env.DB_NAME,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+
+    staging: {
+        client: "mysql",
+        connection: {
+            database: process.env.DB_NAME,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
+    },
+
+    production: {
+        client: "mysql",
+        connection: {
+            database: process.env.DB_NAME,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
+    }
+};
